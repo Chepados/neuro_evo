36e4c551ee8d9aad4945b0b32305ccb59d6cbc1d
Hector_Sancho <iocolopus@gmail.com>
2026-03-23 14:38:31 +0100

    preparar para ampliar con nuevos problemas.

--- Gymnasium/Gymnasium/MLP.py
+++ Gymnasium/Gymnasium/MLP.py
@@ -47,8 +47,7 @@ class MLP:
     def fast_forward (self, x): # fast forward (optimized in time, but not use to train!)
         for i in range(len(self.b)):
             net = np.dot(x,self.W[i]) + self.b[i]
-            x = self.sigm(net)
-        return x
+        return net
     
     def print (self, v, msg=''):
         if self.trace:
@@ -56,7 +55,7 @@ class MLP:
             print(v)
     
     def update (self, x, d, alpha): # do a learning step
-        self.forward(x) # propagate
+        self.forward(x) # propagate\
         
         self.incW = [] # saved in attributes for possible batch updates
         self.incb = []
